9803297c9365613758a0dfc45b83d693bab51c45
Niko Lewis <nikolewis01@gmail.com>
2025-05-17 17:30:41 -0400

    project direction changed and game board complete

--- src/main.js
+++ src/main.js
@@ -1,51 +1,28 @@
-// import samplePupData from "./modules/samplePupData";
-// verifying samplePupData is an array of img urls
-// console.log('sample pup data is', samplePupData);
-
- // Get the container element
-const container =  document.getElementById("photoDisplay")
-
-// looping through data array to display dog images on screen using sample data
 
 
-// samplePupData.forEach(url => {
-//   const img = document.createElement("img"); // Create an image element
-//   img.src = url; // Set the image source
-//   img.alt = "Cute dog photo"; // Set alternative text (accessibility)
-//   img.className = "puppies"
-//   container.appendChild(img); // Add the image to the container
-// });
-
-// //when document loads fetch from api
-//  document.addEventListener("DOMContentLoaded", function () {
-//   fetch('https://dog.ceo/api/breeds/image/random/50').then(resp => resp.json()).then(data => {
-//  console.log(data)
-// }).catch(e => {
-//  console.log(e);
-// })
-
-//  })
 document.addEventListener("DOMContentLoaded", function(){
-  console.log('container is', container)
-  fetch(`https://dog.ceo/api/breeds/image/random/50`)
+  console.log('gameBoard is', gameBoard)
+  
+     fetch(`https://dog.ceo/api/breeds/image/random/9`)
     .then( (resp) => resp.json())
     .then( data => {
       console.log('data is:', data);
       let pupData = data.message;
-      console.log(pupData)
-      pupData.forEach( (pup) => {
-      //possibly add if statement to filter on breeds like hyena etc
-      // setting up image elements for each photo
-      let img = document.createElement("img"); // Create an image element
-      img.src = pup; // Set the image source
-      img.alt = "Cute dog photo"; // Set alternative text (accessibility)
-      img.className = "puppies"
-      console.log('img is', img)
-      container.appendChild(img); //`<li>${character.name}</li>`
+      const imageElements =pupData.map( (pup) => {
+        let gameBoard =  document.getElementById("gameBoard")
+        return `<section class="gridSection"><img class="gridImage" src="${pup}" alt="Cute dog photo"></section>`;
       })
-})
-      .catch(e => {
-console.log(e);
-  })
+      gameBoard.innerHTML = imageElements.join('');
+    })
+    .catch(e => {
+      console.log(e);
+    })
+
+    //another fetch for to populate reference array  of dog breeds
+    
 })
 
+
+
+//functionality: game interactions click event listener open a modal with larger dog image and 4 breed  multiple choice
+// async/ await for win condition  announce win, invite to play again or link to adoption options
