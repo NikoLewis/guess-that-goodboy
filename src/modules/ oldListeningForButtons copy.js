22c793c5b268d676bb535b2781cfdf5222a7a975
Niko Lewis <nikolewis01@gmail.com>
2025-05-20 21:40:05 -0400

    updated evaluation logic and light styling

--- src/modules/ oldListeningForButtons copy.js
+++ src/modules/ oldListeningForButtons copy.js
@@ -6,16 +6,17 @@ function closeDialog() {
     dialog.close();
 }
 
-function listeningForButtons (array, dogName){
+function listeningForButtons (array){
 
-        
+        let gameDogImg = document.querySelector('[data-name]')
+        let winningDog = gameDogImg.getAttribute('data-name')
         // reference ^ const element = document.querySelector('[data-example="someValue"]');
     for (let i = 0 ; i < array.length; i++) {
         let thisButton = array[i]
         //whereever this is be4ing set is where things are going wrong
        
         let buttonDog = thisButton.getAttribute('data-dogname')
-        // console.log('does app know what imgs WINNINGDOG is?', winningDog )
+        console.log('does app know what imgs WINNINGDOG is?', winningDog )
         console.log('does app know what buttons BUTTONDOG means?', buttonDog)
 
 
@@ -31,19 +32,16 @@ function listeningForButtons (array, dogName){
         //     thisButton.setAttribute('data-correctAnswer', 'true') 
         //     console.log('is correct answer marked true?', thisButton)
         // }
-        //     console.log('is correct answer marked true?', thisButton.getAttribute('data-correctAnswer'))
-        //  let usingDataAttrb = thisButton.getAttribute('data-correctAnswer')
+            console.log('is correct answer marked true?', thisButton.getAttribute('data-correctAnswer'))
+         let usingDataAttrb = thisButton.getAttribute('data-correctAnswer')
 
-            // thisButton.setAttribute("data-choiceSelected", "true")
-            // let compareAttributes = thisButton.getAttribute('data-choiceSelected')
+            thisButton.setAttribute("data-choiceSelected", "true")
+            let compareAttributes = thisButton.getAttribute('data-choiceSelected')
             let title = document.getElementById("title");     
-        // let gameDogImg = document.querySelector('[data-name]')
-        // console.log("gameDogImg", gameDogImg);
-        // let winningDog = gameDogImg.getAttribute('data-name')
             // let titleArea = document.getElementById('titleArea')
         //   && usingDataAttrb === 'true'
 // compareAttributes === 'true' && usingDataAttrb === 'true' 
-            if(dogName === buttonDog){
+            if(winningDog === buttonDog){
                 title.className = 'pass'
                 console.log('Correct! Great Job.')
                 title.innerHTML = 'Correct! Great Job.'
@@ -54,7 +52,7 @@ function listeningForButtons (array, dogName){
                 document.getElementById('3').disabled = true;
                 document.getElementById('4').disabled = true;
                      
-                setTimeout(closeDialog, 5000)
+                setTimeout(closeDialog, 2500)
             }else{
                 // console.log('Incorrect! Better luck next time.');
                 title.innerHTML = 'Incorrect! Better luck next time.';
@@ -65,7 +63,7 @@ function listeningForButtons (array, dogName){
                 document.getElementById('2').disabled = true;
                 document.getElementById('3').disabled = true;
                 document.getElementById('4').disabled = true;
-                setTimeout(closeDialog, 5000)
+                setTimeout(closeDialog, 2500)
             }
         })  
     }       
--- src/modules/oldPupGame.js
+++ src/modules/oldPupGame.js
@@ -10,11 +10,13 @@ let dialog = document.getElementById('chosenPupDialog');
 function closeDialog() {
     dialog.close(); 
 }
+
+
 const buildGameBoard = ()=>{
     // console.log('inside build game')
     let inGameBreeds = document.querySelectorAll('.gridImage')
 
-          // console.log('pups included in game are:', inGameBreeds);
+          console.log('pups included in game are:', inGameBreeds);
     
           // For each inGameBreed, adds an event listener that listens for a click event.
           inGameBreeds.forEach((inGameBreed) => {
@@ -33,27 +35,24 @@ const buildGameBoard = ()=>{
     
               let dogName = thisBreed.substring(0, cutOff).replace('-', ' '); // or index + 1 to keep slash
                inGameBreed.setAttribute('data-name', dogName)
-              //  console.log('ingamebreed has attribute?', inGameBreed)
+               console.log('ingamebreed has attribute?', inGameBreed)
               //replace this console.log with a show modal function call
-              // console.log('dog name clicked is', dogName);
+              console.log('dog name clicked is', dogName);
               
-             
+              let multipleChoiceOptions = [dogName]
               
             inGameBreed.addEventListener('click', () => {
-               let multipleChoiceOptions = [dogName]
                 
-                // console.log('ingameBreed =', inGameBreed);
+                console.log('ingameBreed =', inGameBreed);
               
               // console.log('multipleChoiceOptions is', dogName)
-              // const randomElement = dogBreedList[Math.floor(Math.random() * dogBreedList.length)];
+              const randomElement = dogBreedList[Math.floor(Math.random() * dogBreedList.length)];
               // console.log('random element currently is', randomElement)
               // console.log('testing my random dog selecter', randomElement)
               for(let i = 0; i < 3; i++){
                 multipleChoiceOptions.push(dogBreedList[Math.floor(Math.random() * dogBreedList.length)])
                 // console.log(multipleChoiceOptions)
               }
-
-              console.log(multipleChoiceOptions)
     
               
               shuffle(multipleChoiceOptions);
@@ -72,35 +71,32 @@ const buildGameBoard = ()=>{
                 <img src="${whichBreed}" alt="Cute dog picture" />
               </div>
                 <div class="buttons">
-                  <button type="button" id="1" class="btn" data-correctAnswer="false" data-dogName="${multipleChoiceOptions[0]}"  data-choiceSelected="" >${multipleChoiceOptions[0]}</button>
-                  <button type="button" id="2" class="btn" data-correctAnswer="false" data-dogName="${multipleChoiceOptions[1]}" data-choiceSelected="">${multipleChoiceOptions[1]}</button>
-                  <button type="button" id="3" class="btn" data-correctAnswer="false" " data-dogName="${multipleChoiceOptions[2]}" data-choiceSelected="">${multipleChoiceOptions[2]}</button>
-                  <button type="button" id="4" class="btn" data-correctAnswer="false" data-dogName="${multipleChoiceOptions[3]}" data-choiceSelected="">${multipleChoiceOptions[3]}</button>
+                <button type="button" id="1" class="btn" data-correctAnswer="false" data-dogName="${multipleChoiceOptions[0]}"  data-choiceSelected="" >${multipleChoiceOptions[0]}</button>
+                <button type="button" id="2" class="btn" data-correctAnswer="false" data-dogName="${multipleChoiceOptions[1]}" data-choiceSelected="">${multipleChoiceOptions[1]}</button>
+                <button type="button" id="3" class="btn" data-correctAnswer="false" " data-dogName="${multipleChoiceOptions[2]}" data-choiceSelected="">${multipleChoiceOptions[2]}</button>
+                <button type="button" id="4" class="btn" data-correctAnswer="false" data-dogName="${multipleChoiceOptions[3]}" data-choiceSelected="">${multipleChoiceOptions[3]}</button>
                 </div>
               </div>
                 `;
-
-              let buttons = document.getElementsByClassName('btn');
     
-              console.log(buttons);
+    
+              dialog.showModal();
+               let buttons = document.getElementsByClassName('btn');
+              
+    
+    
                for (let i = 0; i < buttons.length; i++) {
                 // console.log('we are within the new for loop')
                 let ourButtons = buttons[i]
                 ourButtons.disabled = false;
                 
                }          
-
+              console.log('I know what buttons means within the img click listener',buttons)
               listeningForButtons(buttons, dogName) 
-    
-              dialog.showModal();
-              
-    
-    
             })
             
             
           });
-
 }
 
 export {buildGameBoard}
